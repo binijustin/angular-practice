0a0a61eceb0b99c596335ef415681ca5ed175787
Gallardo, Justin R <justin.r.gallardo@accenture.com>
2019-05-21 21:23:44 +0800

    add ingredients in recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,38 @@
 import { Recipe } from './recipe.model';
 import { Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
 
 export class RecipeService {
     @Output() selectedRecipe = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Sample Recipe', 'This is a sample recipe', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg'),
-        new Recipe('Another Sample Recipe', 'This is another sample recipe', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg')
+        new Recipe(
+            'Carbonara',
+            'Pasta in white sauce',
+            'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--1001491_11.jpg',
+            [
+                new Ingredient("Pasta", 1),
+                new Ingredient("Cheese", 1),
+                new Ingredient("Bacon", 10)
+            ]),
+        new Recipe(
+            'Eggs Benedict',
+            'Perfect meal for breakfast',
+            'https://shewearsmanyhats.com/wp-content/uploads/2015/04/eggs-benedict-1-480x270.jpg',
+            [
+                new Ingredient("Toast", 2),
+                new Ingredient("Eggs", 2),
+                new Ingredient("Butter", 1),
+                new Ingredient("Ham", 2)
+
+            ])
     ]
 
     getRecipes() {
         return this.recipes.slice();
     }
 
-    setSelectedRecipe(recipe:Recipe){
+    setSelectedRecipe(recipe: Recipe) {
         this.selectedRecipe.emit(recipe);
     }
 
